3a5e961f1eb9acd608219dcd0086bba9cc2fbcbc
Adam Dingle <adam@medovina.org>
2019-10-15 19:42:14 +0200

    Fix reference to MyPacMan class

--- PacMan-vs-Ghosts-Tournament/src/cz/cuni/mff/amis/pacman/tournament/EvaluateAgentConsole.java
+++ PacMan-vs-Ghosts-Tournament/src/cz/cuni/mff/amis/pacman/tournament/EvaluateAgentConsole.java
@@ -286,7 +286,7 @@ public class EvaluateAgentConsole {
 				, "-o", "-pp true -tp 1.0 -gc 4 -lc 20 -v false -2x true -p false -tt 40 -r true"   // prototype-options";
 				, "-c", "50"  // run-count
 				, "-r", "1"  // one-run-repetitions
-				, "-p", "game.controllers.pacman.examples.MyPacMan" // agent-fqcn ... requires MarioAI4J-Agents on classpath!
+				, "-p", "MyPacMan" // agent-fqcn ... requires MarioAI4J-Agents on classpath!
 				, "-i", "MyPacMan"   // agent-id
 				, "-d", "./results" // result-dir"	
 		};
